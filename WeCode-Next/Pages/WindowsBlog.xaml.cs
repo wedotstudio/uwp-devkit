a6785908e41fba867962827f3a56e341bb93ddd3
Patrick J. Wu <wotingwu@live.com>
2017-08-24 16:16:13 +0800

    Windows Blog Fixes Completed

--- WeCode-Next/Pages/WindowsBlog.xaml.cs
+++ WeCode-Next/Pages/WindowsBlog.xaml.cs
@@ -1,35 +1,29 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.IO;
 using System.Linq;
 using System.Net.Http;
-using System.Runtime.InteropServices.WindowsRuntime;
+using System.Net.NetworkInformation;
 using System.Xml.Linq;
 using WeCode_Next.DataModel;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
-using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
-
-// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
 
 namespace WeCode_Next.Pages
 {
-    /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
-    /// </summary>
     public sealed partial class WindowsBlog : Page
     {
         public WindowsBlog()
         {
             this.InitializeComponent();
-            NewsLoad(false);
-            NewsLoad(true);
+            if (NetworkInterface.GetIsNetworkAvailable())
+            {
+                NC.Visibility = Windows.UI.Xaml.Visibility.Collapsed;
+                main.Visibility = Windows.UI.Xaml.Visibility.Visible;
+                NewsLoad(false);
+                NewsLoad(true);
+            }
+            else {
+                NC.Visibility = Windows.UI.Xaml.Visibility.Visible;
+                main.Visibility = Windows.UI.Xaml.Visibility.Collapsed;
+            }
 
         }
 
