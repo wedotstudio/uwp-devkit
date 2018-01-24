5172a598ece6a129e0e37321613c2ff5aefc106d
Patrick J. Wu <wotingwu@live.com>
2018-01-24 00:06:19 -0600

    Dev Shortcut feature almost complete

--- WeCode-Next/Pages/Tools.xaml.cs
+++ WeCode-Next/Pages/Tools.xaml.cs
@@ -3,8 +3,10 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using WeCode_Next.DataModel;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.Storage;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -25,11 +27,39 @@ namespace WeCode_Next.Pages
         public Tools()
         {
             this.InitializeComponent();
+            Loaded += Tools_Loaded;
+        }
+
+        private async void Tools_Loaded(object sender, RoutedEventArgs e)
+        {
+            StorageFile file = await StorageFile.GetFileFromApplicationUriAsync(new Uri("ms-appx:///Assets/Data/tools.csv"));
+            using (var storageStream = await file.OpenReadAsync())
+            {
+                using (Stream stream = storageStream.AsStreamForRead())
+                {
+                    using (StreamReader reader = new StreamReader(stream))
+                    {
+                        List<AccessItem> Items = new List<AccessItem>();
+                        while (!reader.EndOfStream)
+                        {
+                            var line = reader.ReadLine();
+                            var values = line.Split(',');
+                            Items.Add(new AccessItem { Name=values[0], UriStr = new Uri(values[1]), glyph=values[2]});
+                        }
+                        gridView.ItemsSource = Items;
+
+                        reader.Dispose();
+                    }
+                    stream.Dispose();
+                }
+                storageStream.Dispose();
+            }
         }
 
         private void Icon_Click(object sender, ItemClickEventArgs e)
         {
 
         }
+
     }
 }
